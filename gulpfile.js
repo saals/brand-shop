f2f84b18e3475c00b7c3f245d9f50242f6c83a66
saals <salekser@gmail.com>
2022-04-12 12:55:47 +0300

    gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const sourceMaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
 
+const del = require('del');
+
 gulp.task('sass', function () {
   return gulp.src('src/scss/**/*.scss')
     .pipe(plumber())
@@ -39,10 +41,23 @@ gulp.task('html', function () {
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task("serve", gulp.series('html', 'img', 'sass', function () {
+gulp.task('copy', function () {
+  return gulp.src(['src/fonts/**', 'src/js/**'], { base: 'src' })
+    .pipe(gulp.dest('build'));
+
+});
+
+gulp.task('clean', function () {
+  return del('build');
+});
+
+gulp.task("serve", gulp.series('clean', 'copy', 'html', 'img', 'sass', function () {
   browserSync.init({
     server: "build"
   });
   gulp.watch("src/scss/**/*.scss", gulp.parallel("sass"));
   gulp.watch("src/*.html", gulp.parallel("html"));
+
+  gulp.watch("src/img/**/*.{png,jpg,svg}", gulp.parallel("img"));
+  gulp.watch(["src/fonts/**/*.{woff2,woff}", "src/js/**/*.js"], gulp.parallel("copy"));
 }));
